3f568acca475bb5362ea49302b75badb7efbc198
srikk <122169588+py-code314@users.noreply.github.com>
2025-01-22 16:55:52 -0500

    feat: Add JS code for mobile-active design

---
+++ js/main.js
@@ -0,0 +1,32 @@
+const authorByline = document.querySelector('.card__author-byline');
+const socialIcons = document.querySelector('.card__social');
+const shareBtn = document.querySelector('.card__btn');
+const footer = document.querySelector('.card__footer');
+const shareIcon = document.querySelector('.card__share-icon path');
+let isSocial = false;
+
+const toggleSocialIcons = () => {
+  
+  if (isSocial === false) {
+
+    authorByline.style.display = 'none';
+    socialIcons.style.display = 'flex';
+    footer.style.backgroundColor = 'hsl(217, 19%, 35%)';
+    shareBtn.style.backgroundColor = 'hsl(214, 17%, 51%)';
+    shareIcon.setAttribute('fill', 'hsl(0, 0%, 100%)');
+    
+    isSocial = true
+  } else {
+
+    authorByline.style.display = 'flex';
+    socialIcons.style.display = 'none';
+    footer.style.backgroundColor = 'hsl(0, 0%, 100%)';
+    shareBtn.style.backgroundColor = 'hsl(210, 46%, 95%)';
+    shareIcon.setAttribute('fill', '#6E8098');
+    
+    isSocial = false
+  }
+  
+}
+
+shareBtn.addEventListener('click', toggleSocialIcons)
