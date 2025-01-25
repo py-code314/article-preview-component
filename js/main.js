365803a7bfef54f91af87afe775b8a02a335535e
srikk <122169588+py-code314@users.noreply.github.com>
2025-01-24 20:45:23 -0500

    feat: Add CSS & JS code for active states

--- js/main.js
+++ js/main.js
@@ -1,32 +1,36 @@
 const authorByline = document.querySelector('.card__author-byline');
 const socialIcons = document.querySelector('.card__social');
-const shareBtn = document.querySelector('.card__btn');
+const shareBtn = document.querySelector('#card__share-btn');
 const footer = document.querySelector('.card__footer');
 const shareIcon = document.querySelector('.card__share-icon path');
 let isSocial = false;
 
-const toggleSocialIcons = () => {
+// function toggleSocialIcons () {
   
-  if (isSocial === false) {
+//   if (isSocial === false) {
 
-    authorByline.style.display = 'none';
-    socialIcons.style.display = 'flex';
-    footer.style.backgroundColor = 'hsl(217, 19%, 35%)';
-    shareBtn.style.backgroundColor = 'hsl(214, 17%, 51%)';
-    shareIcon.setAttribute('fill', 'hsl(0, 0%, 100%)');
+//     authorByline.style.display = 'none';
+//     socialIcons.style.display = 'flex';
+//     footer.style.backgroundColor = 'hsl(217, 19%, 35%)';
+//     shareBtn.style.backgroundColor = 'hsl(214, 17%, 51%)';
+//     shareIcon.setAttribute('fill', 'hsl(0, 0%, 100%)');
     
-    isSocial = true
-  } else {
+//     isSocial = true
+//   } else {
 
-    authorByline.style.display = 'flex';
-    socialIcons.style.display = 'none';
-    footer.style.backgroundColor = 'hsl(0, 0%, 100%)';
-    shareBtn.style.backgroundColor = 'hsl(210, 46%, 95%)';
-    shareIcon.setAttribute('fill', '#6E8098');
+//     authorByline.style.display = 'flex';
+//     socialIcons.style.display = 'none';
+//     footer.style.backgroundColor = 'hsl(0, 0%, 100%)';
+//     shareBtn.style.backgroundColor = 'hsl(210, 46%, 95%)';
+//     shareIcon.setAttribute('fill', '#6E8098');
     
-    isSocial = false
-  }
+//     isSocial = false
+//   }
   
+// }
+
+function toggleSocialIcons() {
+  shareBtn.setAttribute('aria-expanded', 'true')
 }
 
 shareBtn.addEventListener('click', toggleSocialIcons)
