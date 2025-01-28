af25ecb8a7671c5d263d7379662fc3b1f7acb036
srikk <122169588+py-code314@users.noreply.github.com>
2025-01-28 14:01:36 -0500

    feat: Add accessibility attributes to index
    Add JS code to hide socialMediaContainer when user
    clicks anywhere outside of shareBtn.

--- js/main.js
+++ js/main.js
@@ -1,60 +1,31 @@
-const authorByline = document.querySelector('.card__author-byline');
-const socialIcons = document.querySelector('.card__social');
+// "Content-Type: text/javascript";
+
+const socialMediaIcons = document.querySelector('.card__social');
 const shareBtn = document.querySelector('#card__share-btn');
-const footer = document.querySelector('.card__footer');
-const shareIcon = document.querySelector('.card__share-icon path');
-let isSocial = false;
 
-// function toggleSocialIcons () {
-  
-//   if (isSocial === false) {
 
-//     authorByline.style.display = 'none';
-//     socialIcons.style.display = 'flex';
-//     footer.style.backgroundColor = 'hsl(217, 19%, 35%)';
-//     shareBtn.style.backgroundColor = 'hsl(214, 17%, 51%)';
-//     shareIcon.setAttribute('fill', 'hsl(0, 0%, 100%)');
-    
-//     isSocial = true
-//   } else {
-
-//     authorByline.style.display = 'flex';
-//     socialIcons.style.display = 'none';
-//     footer.style.backgroundColor = 'hsl(0, 0%, 100%)';
-//     shareBtn.style.backgroundColor = 'hsl(210, 46%, 95%)';
-//     shareIcon.setAttribute('fill', '#6E8098');
-    
-//     isSocial = false
-//   }
-  
-// }
-
-function showSocialIcons() {
-  shareBtn.setAttribute('aria-expanded', 'true')
+function showSocialMediaContainer() {
+  shareBtn.setAttribute('aria-expanded', 'true');
+  socialMediaIcons.focus();
 }
 
-shareBtn.addEventListener('click', showSocialIcons);
-
-function closeSocialIcons(event) {
-  // const socialIconsContainer = event.target.closest('.card__social')
+function closeSocialMediaContainer(event) {
   if (event.key === 'Escape') {
-     shareBtn.setAttribute('aria-expanded', 'false');
-  } 
-
-
-}
-  
-function closeSocialDiv(event) {
-if (!socialIcons.contains(event.target) && shareBtn.getAttribute('aria-expanded') === 'true') {
-  shareBtn.setAttribute('aria-expanded', 'false');
+    shareBtn.setAttribute('aria-expanded', 'false');
+    shareBtn.focus();
+  } else if (event.key === 'Enter') {
+    showSocialMediaContainer();
+  }
 }
 
-}
- 
+document.addEventListener('click', (event) => {
+  if (!event.target.closest('#card__share-btn')) {
+    shareBtn.setAttribute('aria-expanded', 'false');
+  } else {
+    // shareBtn.setAttribute('aria-expanded', 'true');
+    showSocialMediaContainer();
+  }
+});
 
-
-
-
-document.addEventListener('keydown', closeSocialIcons)
-// document.addEventListener('click', closeSocialDiv)
-
+shareBtn.addEventListener('click', showSocialMediaContainer);
+document.addEventListener('keydown', closeSocialMediaContainer);
